96689904a772ca1b32c2d202841e766efd328905
nacho.rod91 <juanignacio.rodriguezsilva@cognizant.com>
2020-05-21 10:51:48 +0200

    complete input component render tests for success=false

--- src/Input.js
+++ src/Input.js
@@ -5,9 +5,27 @@ import { connect } from 'react-redux';
 
 export class Input extends Component {
   render() {
+    const contents = this.props.success
+    ? null
+    : (
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          type="text"
+          placeholder="enter guess"
+        />
+        <button
+          data-test="submit-button"
+          className="btn btn-primary mb-2"
+          type="submit">
+          Submit  
+        </button>
+      </form>
+    )
     return (
-      <div>
-        
+      <div data-test="component-input">
+        { contents }
       </div>
     )
   }
