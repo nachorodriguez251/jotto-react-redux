7c378862f3425fb2fac58c28a798184e86d35eb9
nacho.rod91 <juanignacio.rodriguezsilva@cognizant.com>
2020-05-21 09:09:44 +0200

    first commit

---
+++ src/reducers/successReducer.test.js
@@ -0,0 +1,6 @@
+import { actionTypes } from '../actions';
+import { successReducer } from './successReducer';
+
+test('returns default initial state of false when no action is passed', () => {
+
+});
