29ebdc02526116dbe0ff9b0b779a8196b32a40fa
nacho.rod91 <juanignacio.rodriguezsilva@cognizant.com>
2020-05-21 09:22:23 +0200

    write successful tests for successReducer

--- src/reducers/successReducer.test.js
+++ src/reducers/successReducer.test.js
@@ -1,6 +1,12 @@
 import { actionTypes } from '../actions';
-import { successReducer } from './successReducer';
+import successReducer from './successReducer';
 
 test('returns default initial state of false when no action is passed', () => {
+  const newState = successReducer(undefined, {});
+  expect(newState).toBeFalsy();
+});
 
+test('returns state of true upon receibving an action of type CORRECT_GUESS', () => {
+  const newState = successReducer(undefined, { type: actionTypes.CORRECT_GUESS });
+  expect(newState).toBe(true);
 });
