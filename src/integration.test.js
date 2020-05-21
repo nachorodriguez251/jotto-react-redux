b5e95a98bef33189049f5bfbe81a2530cd49cc06
nacho.rod91 <juanignacio.rodriguezsilva@cognizant.com>
2020-05-21 12:47:39 +0200

    set up guessWords tests

---
+++ src/integration.test.js
@@ -0,0 +1,24 @@
+import { storeFactory } from '../test/testUtils';
+import { guessWord } from './actions';
+
+describe('guessWord action dispatcher', () => {
+  describe('no guess words',() => {
+    test('updates state correctly for unsuccessful guess', () => {
+
+    });
+
+    test('updates state correctly for successful guess', () => {
+
+    });
+  });
+
+  describe('some guess words',() => {
+    test('updates state correctly for unsuccessful guess', () => {
+
+    });
+
+    test('updates state correctly for successful guess', () => {
+
+    });
+  });
+});
