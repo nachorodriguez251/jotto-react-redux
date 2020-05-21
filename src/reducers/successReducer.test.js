4f136404291ef60553e6ea7c7d36b24182416427
nacho.rod91 <juanignacio.rodriguezsilva@cognizant.com>
2020-05-21 15:43:36 +0200

    add think for guessedWords piece of state

--- src/reducers/successReducer.test.js
+++ src/reducers/successReducer.test.js
@@ -6,7 +6,7 @@ test('returns default initial state of false when no action is passed', () => {
   expect(newState).toBe(false);
 });
 
-test('returns state of true upon receibving an action of type CORRECT_GUESS', () => {
+test('returns state of true upon receiving an action of type CORRECT_GUESS', () => {
   const newState = successReducer(undefined, { type: actionTypes.CORRECT_GUESS });
   expect(newState).toBe(true);
 });
