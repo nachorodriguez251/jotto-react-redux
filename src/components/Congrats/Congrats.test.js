f7ab3659f7a0164f9a9b491d530492376fff14a1
nachorodriguez251 <nachorodriguez251@gmail.com>
2020-06-25 17:22:41 +0200

    fin prueba

--- src/components/Congrats/Congrats.test.js
+++ src/components/Congrats/Congrats.test.js
@@ -14,7 +14,7 @@ test('renders without error', () => {
   expect(component.length).toBe(1);
 });
 
-test('renders no text when success proP is false', () => {
+test('renders no text when success prop is false', () => {
   const wrapper = setup({ success: false });
   const component = findByTestAttr(wrapper, 'component-congrats');
   expect(component.text()).toBe('');
